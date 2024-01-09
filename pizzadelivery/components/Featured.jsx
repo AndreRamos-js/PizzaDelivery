720f39064bdbe92bca2ec3c2436f258ba320c36d
André Ramos <andreluis0703@gmail.com>
2024-01-09 15:02:06 -0300

    feat: slide featured finalizado

--- pizzadelivery/components/Featured.jsx
+++ pizzadelivery/components/Featured.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from '@/styles/Featured.module.css'
+import styles from '@/styles/Featured.module.css';
+import { useState } from "react";
 
 
 const Featured = () => {
+    const [index, setIndex] = useState(0);
+
     const images = [
         "/img/featured.png",
         "/img/featured2.png",
         "/img/featured3.png"
     ]
 
+    const handleArrow = (direction) =>{
+        if(direction==="l"){
+            setIndex(index !== 0 ? index-1 : 2)
+        }
+        if(direction==="r"){
+            setIndex(index !== 2 ? index+1 : 0)
+        }
+    }
+
   return (
     <div className={styles.container}>
-        <div className={styles.arrowContainer} style={{left:0}}>
-            <Image src="/img/arrowl.png" alt='seta para esquerda' layout='fill'/>
+        <div className={styles.arrowContainer} style={{left:0}} onClick={()=>handleArrow("l")}>
+            <Image src="/img/arrowl.png" alt='seta para esquerda' layout='fill' objectFit="contain"/>
         </div>
 
-        <div className={styles.wrapper}>
+        <div className={styles.wrapper} style={{transform:`translateX(${-100*index}vw)`}}>
             {images.map((img, i) => (
                 <div className={styles.imgContainer} key={i}>
-                    <Image src={img} alt='' layout='fill'></Image>
+                    <Image src={img} alt='imagens de pizzas' layout='fill' objectFit="contain"></Image>
                 </div>
             ))}
         </div>
 
-        <div className={styles.arrowContainer} style={{right:0}}>
-            <Image src="/img/arrowr.png" alt='seta para direita' layout='fill'/>
+        <div className={styles.arrowContainer} style={{right:0}} onClick={()=>handleArrow("r")}>
+            <Image src="/img/arrowr.png" alt='seta para direita' layout='fill' objectFit="contain"/>
         </div>
     </div>
   )
---
+++ pizzadelivery/components/PizzaList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '@/styles/PizzaList.module.css';
+
+const PizzaList = () => {
+  return (
+    <div>PizzaList</div>
+  )
+}
+
+export default PizzaList;
